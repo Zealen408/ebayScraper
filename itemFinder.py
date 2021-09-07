eafe157ad520c41936644d4280c7de044817dd35
Kris <>
2021-09-07 12:47:41 -0700

    Improved code readability.

--- itemFinder.py
+++ itemFinder.py
@@ -3,61 +3,90 @@ import time
 from csv import writer
 from bs4 import BeautifulSoup
 
-def search(searchTerm = 'knife', searchParam = 1):
-    pages = []
-    for loop in range(5):
-        # EBAY PAGE
-        urlAction = f'https://www.ebay.com/sch/i.html?_from=R40&_nkw={searchTerm}&_sacat=0&rt=nc&LH_Auction=1&_ipg=100&_pgn={loop+1}' 
-        urlBuyItNow = f'https://www.ebay.com/sch/i.html?_from=R40&_nkw={searchTerm}&_sacat=0&rt=nc&LH_BIN=1&_ipg=100&_pgn={loop+1}'
-        if searchParam == 1:
-            url = urlAction
-        else:
-            url = urlBuyItNow
-        try:
-            print(str(url))
-            page = requests.get(url)
-            time.sleep(2)
-            print(page.status_code)
-            soup = BeautifulSoup(page.text, 'html.parser')
+def readyURL(pageNum, searchTerm, searchParam = 1):
+    pageNum += 1
+    urlAction = f'https://www.ebay.com/sch/i.html?_from=R40&_nkw={searchTerm}&_sacat=0&rt=nc&LH_Auction=1&_ipg=100&_pgn={pageNum}' 
+    urlBuyItNow = f'https://www.ebay.com/sch/i.html?_from=R40&_nkw={searchTerm}&_sacat=0&rt=nc&LH_BIN=1&_ipg=100&_pgn={pageNum}'
+    if searchParam == 1:
+        url = urlAction
+    else:
+        url = urlBuyItNow
+    return url
+
+def returnSoup(url):
+    page = requests.get(url)
+    if page.status_code == 200:
+        return BeautifulSoup(page.content, 'html.parser')
+    return None
+
+def prepData(item):
+    row_content = []
+    dollars = item.find('span', class_='s-item__price').get_text()
+    # print(dollars)
+    if dollars:
+        dollarsToFloat = float(dollars.strip('$'))
+        if dollarsToFloat <= 10.00:
+            title = item.find('h3', class_ = "s-item__title").get_text()
+            link = item.find('a', class_ = "s-item__link")['href']
+            # shipping = item.find('span', class_="s-item__shipping")
+            # costShipping = float(shipping.strip('+$ shipping'))
+            # costWithShipping = dollarsToFloat + costShipping
+            
+            row_content = [
+                title,
+                dollars,
+                # costWithShipping,
+                link
+            ]
+        return row_content
+    else:
+        return None
+
+
+def main():
+    itemData = []
+    searchTerm = f'{input("Search Term: ") or "knife"}'
+    searchParam = f'{int(input("Enter 2 for Buy Now search") or 1)}'
+
+    for loop in range(2):
+
+        url = readyURL(loop, searchTerm, searchParam)
+        
+        soup = returnSoup(url)
+
+        if soup != None:
 
             items = soup.find_all('div', class_='s-item__wrapper clearfix')
-            print(len(items))
+            # print(len(items))
+            
             for item in items:
                 # print(item)
+            
                 try:
-                    dollars = item.find('span', class_='s-item__price').get_text()
-                    # print(dollars)
-                    dollarsToFloat = float(dollars.strip('$'))
-                    if dollarsToFloat <= 5.00:
-                        title = item.find('h3', class_ = "s-item__title").get_text()
-                        link = item.find('a', class_ = "s-item__link")['href']
-                        # shipping = item.find('span', class_="s-item__shipping")
-                        # costShipping = float(shipping.strip('+$ shipping'))
-                        # costWithShipping = dollarsToFloat + costShipping
-                        
-                        row_content = [
-                            title,
-                            dollars,
-                            # costWithShipping,
-                            link
-                        ]
-
-                        getItemData(row_content)
+            
+                    row_content = prepData(item)
+                    if len(row_content) > 0:
+                        itemData.append(row_content)
+            
                 except Exception as e:
-                    print(f'Error Layer 1: {e}')
-        except Exception:
-            for data in Exception:
-                print(f'Error layer 2: {data}')   
-    return pages
+            
+                    print(f'Error item find: {e}')
+        
+        else:
+            break
+
+    
+    return itemData
 
 
 
-def getItemData(item):
+def saveData(row_content):
     with open("myData.csv",'a', newline='\n') as writeObj:
         csv_writer = writer(writeObj)
-        csv_writer.writerow(item)
+        for row in row_content:
+            csv_writer.writerow(row)
 
 
 
-pages = search()
-
+data = main()
+saveData(data)
